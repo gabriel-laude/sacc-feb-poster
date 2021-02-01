c3740d43ba6a6dd983023b1ee4672a9e73e4ca1c
Gabriel Laude <gabriel.laude@phys.chem.ethz.ch>
2021-02-01 14:50:38 +0100

    instanton 1d implementation added

--- instanton.py
+++ instanton.py
@@ -4,43 +4,63 @@ import quasinewton, path_integral
 from modules import information
 from matplotlib import pyplot as plt
 
-def optimiser(xguess, pes, beta, N, gtol=1e-8):
-    f = len(xguess[0])
-    if f == 2:
-        oldN = len(xguess)
-        if N != oldN:
-            from modules import interpolate
-            xguess = interpolate(xguess, f, N).flatten()
-        traj=path_integral.adiabatic(pes, 1, f=2)
-        x, value = quasinewton.lbfgs(traj.both_trial, xguess.ravel(), gtol=gtol, tau=beta*pes.hbar, maxiter=1000)
+def optimiser(xguess, pes, beta, N, gtol=1e-8, f=2):
+    oldN = len(xguess)
+    if N != oldN:
+        from modules import interpolate
+        xguess = interpolate(xguess, f, N).flatten()
+    traj=path_integral.adiabatic(pes, 1, f=f)
+    x, value = quasinewton.lbfgs(traj.both_trial, xguess.ravel(), gtol=gtol, tau=beta*pes.hbar, maxiter=1000)
 
+    if f==2:
         return x.reshape((len(xguess), f))
+    else:
+        return x
 
-def splitting(x_opt, xmin, beta, pes, vib_ind=[0,0]):
+
+
+
+
+
+def splitting(x_opt, xmin, beta, pes, vib_ind=[0,0], f=2):
     """
     vib_ind is [n_1, n_2].
     """
+    # instanton
     N=len(x_opt)
     tau=np.linspace(0, beta*pes.hbar, int(N))
-    f=2
     V, dVdx, d2Vdx2 = information(x_opt, pes, f=f)
-    traj=path_integral.adiabatic(pes, 1, f=2)
+    traj=path_integral.adiabatic(pes, 1, f=f)
     print('action: ', traj.S(x_opt, tau[-1]))
-    path_min_l = np.full((N, f), -pes.x0)
-    path_min_r = np.full((N, f), pes.x0)
+    
+    # minima 
+    if f == 2:
+        path_min_l = np.full((N, f), -pes.x0)
+        path_min_r = np.full((N, f), pes.x0)
+    
+    if f == 1:
+        path_min_l = np.linspace(-pes.x0, -pes.x0, N)
+        path_min_r = np.linspace(pes.x0, pes.x0, N)
+    
     Vl, dVldx, d2Vldx2 = information(path_min_l, pes, f=f)
     Vr, dVrdx, d2Vrdx2 = information(path_min_r, pes, f=f)
 
     # Build Y
     Y=np.zeros((N*f, N*f))
-    evals_l, Ul = linalg.eigh(pes.hessian(x_opt[0]))
-    evals_r, Ur = linalg.eigh(pes.hessian(x_opt[-1]))
-    omega_l, omega_r = np.sqrt(evals_l), np.sqrt(evals_r)
-    Xl=Xr=np.zeros((f,f))
-    Xl[0,0], Xl[1,1] = omega_l[0], omega_l[1]
-    Xr[0,0], Xr[1,1] = omega_r[0], omega_r[1]
-    Y[:f,:f]=np.linalg.multi_dot([Ul, Xl, Ul.T])
-    Y[(N-1)*f:,(N-1)*f:]=np.linalg.multi_dot([Ur, Xr, Ur.T])
+
+    if f == 1:
+        omega_l, omega_r = np.sqrt(pes.hessian(x_opt[0])), np.sqrt(pes.hessian(x_opt[-1]))
+        Y[0,0]=omega_l
+        Y[-1,-1]=omega_r
+    if f == 2:
+        evals_l, Ul = linalg.eigh(pes.hessian(x_opt[0]))
+        evals_r, Ur = linalg.eigh(pes.hessian(x_opt[-1]))
+        omega_l, omega_r = np.sqrt(evals_l), np.sqrt(evals_r)
+        Xl=Xr=np.zeros((f,f))
+        Xl[0,0], Xl[1,1] = omega_l[0], omega_l[1]
+        Xr[0,0], Xr[1,1] = omega_r[0], omega_r[1]
+        Y[:f,:f]=np.linalg.multi_dot([Ul, Xl, Ul.T])
+        Y[(N-1)*f:,(N-1)*f:]=np.linalg.multi_dot([Ur, Xr, Ur.T])
 
     # alpha factors
     alpha_left = omega_l / pes.hbar
@@ -49,13 +69,17 @@ def splitting(x_opt, xmin, beta, pes, vib_ind=[0,0]):
     # build A
     A = traj.d2Sdx2(x_opt, tau[-1]) + Y
     B=linalg.inv(A)
-    from scipy.sparse.linalg import eigsh as eigvalser
     evals_A = linalg.eigvalsh(A)
 
     # build A0
     Yl=Yr=Y
-    Yl[(N-1)*f:, (N-1)*f:] = Y[:f, :f]
-    Yr[:f, :f] = Y[(N-1)*f:, (N-1)*f:]
+    if f==2:
+        Yl[(N-1)*f:, (N-1)*f:] = Y[:f, :f]
+        Yr[:f, :f] = Y[(N-1)*f:, (N-1)*f:]
+    if f==1:
+        Yl[-1,-1]=Y[0,0]
+        Yr[0,0]=Y[-1,-1]
+    
     A0l=traj.d2Sdx2(path_min_l, tau[-1]) + Yl
     A0r=traj.d2Sdx2(path_min_r, tau[-1]) + Yr
 
@@ -68,8 +92,12 @@ def splitting(x_opt, xmin, beta, pes, vib_ind=[0,0]):
     Phi = np.sqrt(beta*pes.hbar/N * np.exp(logdet_A - 0.5*(logdet_Al + logdet_Ar)))
     theta0=np.sqrt(S / (2*np.pi*pes.hbar)) / Phi * np.exp(-S / pes.hbar)
     delta0=2*pes.hbar*theta0
-    if 0:
+    if 1:
         print('determinants: ', linalg.det(A0l), linalg.det(A0r), np.prod(evals_A[1:]))
+        print('logdets: ', logdet_Al, logdet_Ar, logdet_A)
+        print('exp logdets: ', np.exp(logdet_A - 0.5*(logdet_Al + logdet_Ar)))
+        print('tau_N: ', beta/N*pes.hbar)
+        print('beta: ', beta)
         print("Phi is: ", Phi) 
         print('theta0, delta0: ', theta0, 2*pes.hbar*theta0)
 
@@ -83,13 +111,18 @@ def splitting(x_opt, xmin, beta, pes, vib_ind=[0,0]):
         ratio_perp=2*np.sqrt(alpha_left[0] * alpha_right[0]) * np.exp(beta*pes.hbar*omega_l[0]) * B[1,-1] * pes.hbar
         ratio20=4*alpha_left[0]*alpha_right[0]*np.exp(2 * beta*pes.hbar*omega_l[0])*pes.hbar**2*B[1,-1]**2
 
-    if 1:
+    if f == 1:
+        ratio1=xi * xf / (np.sqrt(Bl[0,-2]*Br[0,-2])*pes.hbar)
+        ratio2=ratio1**2/2
+        print('ground state, ratio1, ratio2: ', theta0*2, ratio1, ratio2)
+    
+    if 1 and f==2:
         ratio_parallel= xi[0] * xf[0] / (np.sqrt(Bl[0,-2]*Br[0,-2])*pes.hbar)
         ratio_perp=B[1,-1] / np.sqrt(Bl[1,-1] * Br[1,-1])
         ratio11=B[1,-1]*xi[0]*xf[0] / (pes.hbar*Bl[0,-2]*Bl[1,-1]) #+ 2*Bl[0,-1]**2)
         ratio20=B[1,-1]**2 / np.sqrt(Bl[1,-1]**2*Br[1,-1]**2)
         #print("ratio20, splitting20: ", ratio20, ratio20*pes.hbar*theta0*2, ratio_perp)
-    return theta0*pes.hbar, pes.hbar*theta0*ratio_perp, pes.hbar*theta0*np.abs(ratio_parallel), np.abs(ratio11)*theta0*pes.hbar, np.abs(ratio20)*theta0*pes.hbar, 0
+        return theta0*pes.hbar, pes.hbar*theta0*ratio_perp, pes.hbar*theta0*np.abs(ratio_parallel), np.abs(ratio11)*theta0*pes.hbar, np.abs(ratio20)*theta0*pes.hbar, 0
 
 
 
@@ -145,3 +178,29 @@ if 0:
     x_opt=optimiser(xguess, pes, beta, N)
     pes.plot(trajectory=x_opt)
     splitting(x_opt, xmin, beta, pes)
+
+
+if 1:
+    from potentials import AsymDW
+    V0=2
+    x0=5*np.sqrt(V0)
+    a=1e-7/x0**2
+    b=1e-7/x0
+    a=b=0
+    pes=AsymDW(V0, x0, a, b)
+    N=256
+    beta=80
+    
+    if 0: 
+        xguess=np.linspace(-pes.x0, pes.x0, N)
+    if 1:
+        tau=np.linspace(0, beta*pes.hbar, N)
+        omega0=np.sqrt(pes.hessian(pes.x0))
+        xguess=np.array([-pes.x0*np.tanh(omega0/2*(tau[i]-tau[N//2])) for i in range(N)])
+    
+    x_opt=optimiser(xguess, pes, beta, N, f=1, gtol=1e-6)
+
+    #print(x_opt)
+    pes.plot(trajectory=x_opt)
+    plt.show()
+    splitting(x_opt, x0, beta, pes, f=1)
